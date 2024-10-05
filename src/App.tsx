a721f854ed32c6fdc86169b5130d1c730c7b183a
subinsong01 <haely97@naver.com>
2024-10-05 22:26:10 +0900

    feat: set up folder structure & apply basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainPage from './pages/index'
-import AboutPage from './pages/about';
+import MainPage from "./pages/index/index"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path='/' element={<MainPage />}></Route>
-        <Route index path='/about' element={<AboutPage />}></Route>
-        <Route index path='/about/:id' element={<AboutPage />}></Route>
       </Routes>
     </BrowserRouter>
   )
